9b583cd0653dcc2ba0e37e42457ca7712b19f314
Marta Amani <marta.amani10@gmail.com>
2026-04-22 17:48:48 +0000

    Wed, Apr 22, 2026, 1:48 PM -04:00

--- FP.py
+++ FP.py
@@ -149,7 +149,7 @@ def main():
             print(f"{'Recipe:':<{max_len}} {recipe.get('Recipe_Name')}")
             print(textwrap.fill(recipe.get('Ingredient_List').rstrip(',)'), width=80, initial_indent="Ingredients: ",subsequent_indent=" " * 13))
             print(f"{'Calories:':<{max_len}} {recipe.get('Calories')}")
-            print(f"{'Allergens:':<{max_len}} {recipe.get('Allergens').rstrip(',)')}")
+            print(f"{'Allergens:':<{max_len}} {recipe.get('Allergens').rstrip(', ')}")
             break
 
 if __name__ == "__main__":
