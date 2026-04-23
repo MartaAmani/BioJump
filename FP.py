671f761abea5c0007385d250ab36ed04dd50139c
Marta Amani <marta.amani10@gmail.com>
2026-04-23 03:02:24 +0000

    Wed, Apr 22, 2026, 11:02 PM -04:00

--- FP.py
+++ FP.py
@@ -37,37 +37,7 @@ def partial_match(search, recipe_name):
     return any(word in recipe_words for word in search_words)
 
 def get_ingredients(recipe_name):
-    print('Searching recipe from the Harvard Univeristy Dining Service Website\n')
 
-    # craft a request
-    url = f"https://go.apis.huit.harvard.edu/ats/dining/v3/recipes"
-
-    params = {"name": recipe_name}
-    headers = {
-        "User-Agent": "FP Project Cs 32",
-        "X-Api-Key": "reC5wGF3ZYFyQQodPHKXwelidEpVnir8EJUD6DDadGnT6J7S"
-    }
-
-    response = requests.get(url, params=params, headers=headers)
-
-    # DEBUG
-    # print(f"Status code: {response.status_code}")
-    # print(f"URL called: {response.url}")
-    # print(f"Raw response: {response.text[:500]}")
-
-    if response.status_code != 200:
-        print("Error: bad response from server")
-        return None
-
-    if not response.text.strip():
-        print("Error: empty response")
-        return None
-
-    data = response.json() # parse a JSON-formatted API response into a native Python data structure
-
-    if len(data) == 0:
-        print(f"No recipes found for '{recipe_name}'")
-        return None
 
     search = recipe_name.upper() # all the names are uppercase
 
