20771fc0f53c5621b9d4bf6e45ffd2f055ff5cd2
Marta Amani <marta.amani10@gmail.com>
2026-04-28 00:58:48 +0000

    Mon, Apr 27, 2026, 8:58 PM -04:00

--- FP.py
+++ FP.py
@@ -403,7 +403,7 @@ def main():
     #prefereces = dietary_preference()
 
     while True:
-        search = input("Which recipe would you like to search? (or 'q' to quit): ").strip().lower() # we strip so that we can still look for a math
+        search = input("Which recipe would you like to search? (or 'q' to quit): ").strip().lower() # we strip so that we can still look for a match even
                                                                                             # if the user added a space at the beginnig by accident
         if search.lower() == "q" or search.lower() == "quit" or search.lower() == "no" or search.lower() == "n"  :
             console.print("\nGoodbye! ")
